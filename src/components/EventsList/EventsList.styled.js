032772d2ab52cc390bf21b1ae647f4e8484fbfaa
Adina <adiella235@gmail.com>
2022-03-15 17:25:45 +0200

    Load events list and show details for selected event

--- src/components/EventsList/EventsList.styled.js
+++ src/components/EventsList/EventsList.styled.js
@@ -8,27 +8,46 @@ export const StyledEventsList = styled.div`
 `;
 
 export const AllEventsAndActions = styled.div`
-  // background-color: pink;
-  // width: 40%;
-  width: 100%;
+  width: 55%;
 `;
-
-export const StyledActions = styled.div``;
 
 export const SortFilter = styled.div`
   padding: 1.5em 0;
   display: flex;
-  // justify-content: center;
-  // align-items: center;
 `;
 
 export const AllEvents = styled.div``;
 
 export const StyledDetails = styled.div`
-  // width: 55%;
-  // border-left: 1px solid blue;
-  // border-left: 1px solid ${colors.neutralDark}
+  width: 40%;
+
+  margin-right: 1em;
   margin-left: 5%;
-  padding: 0 0 3em 3em;
-  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.26);
+  padding: 1em 2em;
+  border-radius: 6px;
+  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+    rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+
+  h3 {
+    color: ${colors.primary};
+    font-weight: 600;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    padding: 0.25em 0;
+
+    .more {
+      margin-left: 1em;
+    }
+
+    .icon {
+      margin-right: 0.5em;
+    }
+  }
+
+  .details-header {
+    margin-bottom: 1em;
+  }
 `;
